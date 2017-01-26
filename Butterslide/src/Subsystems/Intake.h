22b7def131042c6358997a56b8c7d5498fbad536
CIS 4607 <beckerrobo@gmail.com>
2017-01-25 20:20:45 -0600

    Tested all code on robot - almost ready!
    needs drive deadzones
    needs feeder motor fixed (not working)
    add reverse buttons!

--- Butterslide/src/Subsystems/Intake.h
+++ Butterslide/src/Subsystems/Intake.h
@@ -8,11 +8,12 @@ class Intake : public Subsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
+	std::shared_ptr<CANTalon> loader;
+	std::shared_ptr<CANTalon> sucker;
 
 public:
 	Intake();
-	std::shared_ptr<CANTalon> loader;
-	std::shared_ptr<CANTalon> sucker;
+
 	void InitDefaultCommand();
 	void FeederBarSetSpeed(float);
 	void FloorIntakeSpeed(float);
