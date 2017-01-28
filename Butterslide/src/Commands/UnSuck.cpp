5939c1a3146ad68be32600fe8142bb9e91ad0413
Logan Steffen <ultimate360solutions@gmail.com>
2017-01-28 14:53:25 -0600

    Fixed outake command

--- Butterslide/src/Commands/UnSuck.cpp
+++ Butterslide/src/Commands/UnSuck.cpp
@@ -24,7 +24,7 @@ void UnSuck::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool UnSuck::IsFinished() {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
